66869a1a64af7ef3d39e71476377ec099e62cb5a
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-05-26 19:08:26 +0100

    write boilerplate around a function that operates on arrays of pixels, using BMP images as input and output for ease of testing. in its current iteration, this function simply halves each pixel brightness value. later, the boilerplate will be removed and Dart will directly execute this function.

--- main.c
+++ main.c
@@ -1,53 +1,103 @@
 /**************************************************************
 
-        The program reads an BMP image file and creates a new
-        image that is the negative of the input file.
+The program takes as input a BMP image file,
+runs `do_something` on the image content,
+and saves the result as another BMP image.
 
 **************************************************************/
 
 #include "qdbmp.h"
-#include <stdio.h>
 
-/* Creates a negative image of the input bitmap file */
+#include <complex.h>
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// This is where the real logic happens!
+// The rest of this file is purely boilerplate.
+// Later, Dart will execute this function directly.
+unsigned char *do_something(unsigned char *pixel_array, size_t width,
+                            size_t height) {
+  // assume the input pixel array has this length
+  size_t length = width * height * 3;
+
+  // TODO: ensure I am using `malloc` correctly after I learn about pointers
+  unsigned char *pixel_array_output = malloc(sizeof(unsigned char[length]));
+
+  // placeholder logic, just a test
+  for (size_t i = 0; i < length; ++i) {
+    pixel_array_output[i] = pixel_array[i] / 2;
+  }
+
+  return pixel_array_output;
+}
+
 int main(int argc, char *argv[]) {
   unsigned char r, g, b;
   unsigned long int width, height;
   unsigned long int x, y;
   BMP *bmp;
 
-  /* Check arguments */
+  // check arguments
   if (argc != 3) {
     fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
     return 0;
   }
 
-  /* Read an image file */
+  // read an image file
   bmp = BMP_ReadFile(argv[1]);
   BMP_CHECK_ERROR(stdout, -1);
 
-  /* Get image's dimensions */
+  // get image dimensions
   width = BMP_GetWidth(bmp);
   height = BMP_GetHeight(bmp);
 
   printf("width %lu\n", width);
   printf("height %lu\n", height);
 
-  /* Iterate through all the image's pixels */
+  // allocate memory for the input pixel array,
+  // and a pointer for stepping through it
+  unsigned char pixel_array[width * height * 3];
+  unsigned long int pixel_array_index = 0;
+
+  // read the input image into the pixel array
   for (x = 0; x < width; ++x) {
     for (y = 0; y < height; ++y) {
-      /* Get pixel's RGB values */
+      // read the pixel's RGB values from the BMP image
       BMP_GetPixelRGB(bmp, x, y, &r, &g, &b);
 
-      /* Invert RGB values */
-      BMP_SetPixelRGB(bmp, x, y, 255 - r, 255 - g, 255 - b);
+      // populate three elements of the pixel array
+      pixel_array[pixel_array_index] = r;
+      pixel_array[pixel_array_index + 1] = g;
+      pixel_array[pixel_array_index + 2] = b;
+      pixel_array_index += 3;
     }
   }
 
-  /* Save result */
+  // EXECUTE LOGIC on pixel array data,
+  // saving the output in another pixel array
+  unsigned char *pixel_array_output = do_something(pixel_array, width, height);
+
+  // write the output pixel array to an image
+  pixel_array_index = 0;
+  for (x = 0; x < width; ++x) {
+    for (y = 0; y < height; ++y) {
+      // read the pixel's RGB values from the output pixel array
+      r = pixel_array_output[pixel_array_index];
+      g = pixel_array_output[pixel_array_index + 1];
+      b = pixel_array_output[pixel_array_index + 2];
+      pixel_array_index += 3;
+
+      // write result to BMP
+      BMP_SetPixelRGB(bmp, x, y, r, g, b);
+    }
+  }
+  // save result
   BMP_WriteFile(bmp, argv[2]);
   BMP_CHECK_ERROR(stdout, -2);
 
-  /* Free all memory allocated for the image */
+  // free all memory allocated for the image
   BMP_Free(bmp);
 
   return 0;
